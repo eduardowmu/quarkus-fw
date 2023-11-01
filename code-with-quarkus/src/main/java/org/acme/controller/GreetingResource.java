c0a31b9b71aa0d6c8b0c63375751c7b992f15003
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2023-11-01 00:21:56 -0300

    primeiras classes de entidade e repository

--- code-with-quarkus/src/main/java/org/acme/controller/GreetingResource.java
+++ code-with-quarkus/src/main/java/org/acme/controller/GreetingResource.java
@@ -1,4 +1,4 @@
-package org.acme;
+package org.acme.controller;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
---
+++ code-with-quarkus/src/main/java/org/acme/model/Customer.java
@@ -0,0 +1,23 @@
+package org.acme.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String lasName;
+    private LocalDate birthDate;
+    private String email;
+}
